403c9034737dff003b4d766bb9930dc15531e972
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2021-11-18 10:03:22 +0100

    Create mapping specification in AbstractTransformerTest.

--- src/test/java/com/sap/dsc/aas/lib/transform/AbstractTransformerTest.java
+++ src/test/java/com/sap/dsc/aas/lib/transform/AbstractTransformerTest.java
@@ -5,6 +5,26 @@
  */
 package com.sap.dsc.aas.lib.transform;
 
+import static org.mockito.Mockito.mock;
+
+import io.adminshell.aas.v3.dataformat.core.ReflectionHelper;
+import io.adminshell.aas.v3.model.AssetAdministrationShell;
+import io.adminshell.aas.v3.model.AssetAdministrationShellEnvironment;
+import io.adminshell.aas.v3.model.AssetInformation;
+import io.adminshell.aas.v3.model.Key;
+import io.adminshell.aas.v3.model.KeyElements;
+import io.adminshell.aas.v3.model.KeyType;
+import io.adminshell.aas.v3.model.Property;
+import io.adminshell.aas.v3.model.Reference;
+import io.adminshell.aas.v3.model.Submodel;
+import io.adminshell.aas.v3.model.SubmodelElementCollection;
+import io.adminshell.aas.v3.model.impl.DefaultKey;
+import io.adminshell.aas.v3.model.impl.DefaultReference;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
@@ -12,7 +32,14 @@ import org.dom4j.Namespace;
 import org.dom4j.QName;
 
 import com.sap.dsc.aas.lib.TestUtils;
+import com.sap.dsc.aas.lib.expressions.ConstantExpr;
+import com.sap.dsc.aas.lib.expressions.XPathExpr;
+import com.sap.dsc.aas.lib.mapping.model.BindSpecification;
+import com.sap.dsc.aas.lib.mapping.model.Header;
+import com.sap.dsc.aas.lib.mapping.model.LegacyTemplate;
+import com.sap.dsc.aas.lib.mapping.model.LegacyTemplateSupport;
 import com.sap.dsc.aas.lib.mapping.model.MappingSpecification;
+import com.sap.dsc.aas.lib.mapping.model.Template;
 import com.sap.dsc.aas.lib.transform.validation.PreconditionValidator;
 
 public abstract class AbstractTransformerTest {
@@ -32,9 +59,113 @@ public abstract class AbstractTransformerTest {
     protected PreconditionValidator mockPreconditionValidator;
 
     protected void setUp() throws Exception {
-		TestUtils.resetBindings();
+        TestUtils.resetBindings();
         // sample AML document
         this.document = createDocument();
+        // sample mapping specification
+        this.mapping = createMappingSpecification();
+        this.mockPreconditionValidator = mock(PreconditionValidator.class);
+    }
+
+    protected <T> T createTemplate(Class<T> modelClass) {
+        T target;
+        try {
+            target = ReflectionHelper.getDefaultImplementation(modelClass).getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+
+        // create a proxy instance that implements the bean interface and the config interface
+        List<Class<?>> interfaces = new ArrayList<>();
+        interfaces.addAll(Arrays.asList(target.getClass().getInterfaces()));
+        interfaces.add(LegacyTemplate.class);
+        LegacyTemplate config = new LegacyTemplateSupport(target);
+        return (T) Proxy.newProxyInstance(getClass().getClassLoader(),
+            interfaces.toArray(new Class<?>[interfaces.size()]),
+            (o, method, args) -> {
+                try {
+                    // route to concrete object - either template definition or the underlying bean
+                    if (Template.class.isAssignableFrom(method.getDeclaringClass())) {
+                        return method.invoke(config, args);
+                    }
+                    return method.invoke(target, args);
+                } catch (Throwable e) {
+                    // can be used to set a breakpoint if something goes wrong
+                    throw e;
+                }
+            });
+    }
+
+    protected Reference createReference(String value, KeyType idType, KeyElements type) {
+        List<Key> keys = new ArrayList<>();
+        Key key = new DefaultKey();
+        key.setIdType(idType);
+        key.setType(type);
+        key.setValue(value);
+        keys.add(key);
+        Reference reference = new DefaultReference();
+        reference.setKeys(keys);
+        return reference;
+    }
+
+    protected Reference createSemanticId(String semanticIdStr) {
+        return createReference(semanticIdStr, KeyType.IRDI, KeyElements.CONCEPT_DESCRIPTION);
+    }
+
+    protected MappingSpecification createMappingSpecification() {
+        this.mapping = new MappingSpecification();
+        mapping.setHeader(new Header());
+
+        AssetAdministrationShellEnvironment aasEnv = createTemplate(AssetAdministrationShellEnvironment.class);
+        mapping.setAasEnvironmentMapping(aasEnv);
+        ((Template) aasEnv).setForeachExpression(new XPathExpr(Arrays.asList(
+            new ConstantExpr(
+                "//caex:SystemUnitClass[caex:SupportedRoleClass/"
+                    + "@RefRoleClassPath='AutomationMLComponentStandardRCL/AutomationComponent']"))));
+        BindSpecification bindSpec = new BindSpecification();
+        bindSpec.setBinding("idShort", new XPathExpr(Arrays.asList(new ConstantExpr("@Name"))));
+
+        // aasEnv.setIdGeneration(createSimpleIdGeneration(ID_VALUE));
+        // aasEnv.setConfigElementId(DEFAULT_CONFIG_ELEMENT_ID);
+
+        AssetAdministrationShell assetShell = createTemplate(AssetAdministrationShell.class);
+        bindSpec = new BindSpecification();
+        bindSpec.setBinding("idShort", new XPathExpr(Arrays.asList(new ConstantExpr("'ShellIdShort'"))));
+        // assetShell.setIdGeneration(createSimpleIdGeneration("5678"));
+
+        aasEnv.setAssetAdministrationShells(Arrays.asList(assetShell));
+
+        AssetInformation assetInformation = createTemplate(AssetInformation.class);
+        assetInformation.setGlobalAssetId(createReference("SAMPLE_ID_MUST_COME_FROM_XPATH", KeyType.CUSTOM, KeyElements.ASSET));
+        assetShell.setAssetInformation(assetInformation);
+
+        Submodel subModel = createTemplate(Submodel.class);
+        ((Template) subModel).setForeachExpression(new XPathExpr(Arrays.asList(new ConstantExpr("caex:Attribute[@Name='MyAttribute']"))));
+        subModel.setSemanticId(createSemanticId("mySemanticId"));
+        // subModel.setIdGeneration(createSimpleIdGeneration(ID_VALUE + "_submodel"));
+
+        Property submodelElementProperty = createTemplate(Property.class);
+        ((Template) subModel).setForeachExpression(new XPathExpr(Arrays.asList(new ConstantExpr("caex:Attribute[@Name='MySubAttribute']"))));
+        submodelElementProperty.setValueType("String");
+
+        subModel.setSubmodelElements(Arrays.asList(submodelElementProperty));
+
+        Submodel submodelMultipleAttrs = createTemplate(Submodel.class);
+        ((Template) submodelMultipleAttrs).setForeachExpression(new XPathExpr(Arrays.asList(new ConstantExpr("caex:Attribute"))));
+
+        Property submodelElementMultiple = createTemplate(Property.class);
+        ((Template) submodelElementMultiple).setForeachExpression(new XPathExpr(Arrays.asList(new ConstantExpr("caex:Attribute"))));
+        submodelElementProperty.setValueType("String");
+
+        submodelMultipleAttrs.setSubmodelElements(Arrays.asList(submodelElementMultiple));
+
+        SubmodelElementCollection submodelElementCollection = createTemplate(SubmodelElementCollection.class);
+        // ((Template) submodelElementCollection).setForeachExpression(new XPathExpr(Arrays.asList(new ConstantExpr(""));
+        submodelElementCollection.setValues(Arrays.asList(submodelElementProperty, submodelElementMultiple));
+
+        aasEnv.setSubmodels(Arrays.asList(subModel, submodelMultipleAttrs));
+
+        return mapping;
     }
 
     // Create a sample AML document (xml) for use in testing
