6833438fc7139bbacca26e7280015be7db9f8860
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2021-11-23 16:37:58 +0100

    Fixed NPE if submodel.getIdentification() is null.

--- src/main/java/com/sap/dsc/aas/lib/transform/postprocessor/AutoWireSubmodels.java
+++ src/main/java/com/sap/dsc/aas/lib/transform/postprocessor/AutoWireSubmodels.java
@@ -1,7 +1,5 @@
 package com.sap.dsc.aas.lib.transform.postprocessor;
 
-import java.util.function.Consumer;
-
 import io.adminshell.aas.v3.model.AssetAdministrationShell;
 import io.adminshell.aas.v3.model.AssetAdministrationShellEnvironment;
 import io.adminshell.aas.v3.model.Key;
@@ -13,30 +11,30 @@ import io.adminshell.aas.v3.model.impl.DefaultKey;
 import io.adminshell.aas.v3.model.impl.DefaultReference;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * PostProcessor which adds each {@link Submodel} as a {@link Reference} to each
- * {@link AssetAdministrationShell} of the same
- * {@link AssetAdministrationShellEnvironment}
- * 
- * @author br-iosb
+ * PostProcessor which adds each {@link Submodel} as a {@link Reference} to each {@link AssetAdministrationShell} of the same {@link
+ * AssetAdministrationShellEnvironment}
  *
+ * @author br-iosb
  */
 public class AutoWireSubmodels implements Consumer<AssetAdministrationShellEnvironment> {
 
-	@Override
-	public void accept(AssetAdministrationShellEnvironment t) {
-		ArrayList<Reference> smRefs = t.getSubmodels().stream()
-				.map(submodel -> createReference(submodel.getIdentification().getIdentifier(), KeyElements.SUBMODEL,
-						KeyType.CUSTOM))
-				.collect(Collectors.toCollection(ArrayList::new));
-		t.getAssetAdministrationShells().forEach(aas -> aas.getSubmodels().addAll(smRefs));
-	}
+    @Override
+    public void accept(AssetAdministrationShellEnvironment t) {
+        ArrayList<Reference> smRefs = t.getSubmodels().stream()
+            .flatMap(submodel -> submodel.getIdentification() == null ? Stream.empty() :
+                Stream.of(createReference(submodel.getIdentification().getIdentifier(), KeyElements.SUBMODEL, KeyType.CUSTOM)))
+            .collect(Collectors.toCollection(ArrayList::new));
+        t.getAssetAdministrationShells().forEach(aas -> aas.getSubmodels().addAll(smRefs));
+    }
 
-	protected Reference createReference(String value, KeyElements keyElement, KeyType keyType) {
-		Key key = new DefaultKey.Builder().idType(keyType).type(keyElement).value(value).build();
+    protected Reference createReference(String value, KeyElements keyElement, KeyType keyType) {
+        Key key = new DefaultKey.Builder().idType(keyType).type(keyElement).value(value).build();
 
-		return new DefaultReference.Builder().keys(Arrays.asList(key)).build();
-	}
+        return new DefaultReference.Builder().keys(Arrays.asList(key)).build();
+    }
 }
